0c41fcb0171817e3f44f56d7179d71873b4c892a
Andrew Collier <andrew@fathomdata.dev>
2024-02-27 17:25:04 +0000

    test: Simplify paragraphSplit()

--- theme/hbs-helpers/paragraph-split.js
+++ theme/hbs-helpers/paragraph-split.js
@@ -1,16 +1,12 @@
 const { SafeString } = require('handlebars');
 const markdown = require('markdown-it')();
 
-const paragraphSplit = (text) => {
-  const expr = /\r\n|\r|\n/g;
+const paragraphSplit = (text) => {  
+    if (text != null) {
+        text = markdown.render(text);
+    }
 
-  if (text != null) {
-    text = markdown.render(text);
-  }
-
-  const lines = Array.isArray(text) ? text.join('').split(expr) : text.split(expr);
-  const output = lines.filter(line => line).reduce((a, b) => `${a}<p>${b}</p>`, '');
-  return new SafeString(output);
+    return new SafeString(text);
 };
 
 module.exports = { paragraphSplit };
